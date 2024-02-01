97aacc2f204e11d673a744a1ff25a541792b6707
estebonbon <estesarmiento1@gmail.com>
2024-01-31 20:10:14 -0500

    Added an event listener and functions to the (Show pet status) button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
+
+// ============================================== CHALLENGE 1 PART A ================================================ //
 const createPet = function (name, species) {
     const pet = {
         name: name,       // the key and value must be duplicates in order for this to work properly
@@ -25,9 +27,9 @@ const createPet = function (name, species) {
     return pet;
 };
 
-const sora = createPet("Sora", "Ferret");
+const sora = createPet("Sora", "ferret");
 
-const clover = createPet("Clover", "Rabbit");
+const clover = createPet("Clover", "rabbit");
 
 const baxter = createPet("Bacter", "hamster");
 
@@ -41,3 +43,34 @@ clover.sleep(); // createPet is not needed to cause the method to act
 baxter.play(); // createPet is not needed to cuase the method to act
 
 console.log(clover, baxter);
+
+// ============================================== CHALLENGE 1 PART B ================================================ //
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+console.log(allPets);
+
+const showPets = function (petArray) { 
+    pets.innerHTML = ""; // This will clear your list whenever showPets is run, so that you can update it with fresh info.
+    
+    for (let pet of petArray) { // pet could literally be any animal, pet is like the access to the key-values, and petArray is like the access to a specific animal in showPets.
+        let status = "ready to play"; // Instead of coding an else value, we can pre-assign a value that will only change if the "if statement" conditions are met.
+        if (pet.isTired >= 7) {
+            let status = "sleeping";
+        };
+
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li); // for the append no quotation marks are necessary.
+
+    }
+};
+
+statusButton.addEventListener("click", function () {
+    showPets(allPets); // Here showPets is a function inside of a function being called only if the button is clicked. All pets is an Array, and will become an individual object of the array once it enters the (for of loop)
+    }
+);
+
